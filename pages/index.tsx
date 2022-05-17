0124cee761ccb7fac09d5465e73e77579ef2dfa0
sarahshkb <sarah.shkb@gmail.com>
2022-05-18 01:47:45 +0430

    Migrate to Next.js

---
+++ pages/index.tsx
@@ -0,0 +1,20 @@
+import type { NextPage } from 'next'
+import Head from 'next/head'
+import MainMenu from '../components/MainMenu'
+
+const Home: NextPage = () => {
+  return (
+    <div>
+      <Head>
+        <title>HOKM</title>
+        <meta name="description" content="a mystrious Persian game!" />
+        <link rel="icon" href="/icon.png" />
+      </Head>
+      <main>
+        <MainMenu/>
+      </main>
+    </div>
+  )
+}
+
+export default Home
